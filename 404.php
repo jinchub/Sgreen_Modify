81d4206a3919bde2906689cd2c8d48d53f8f8ba8
jinc <41d_1054542382_jinc@git.cloud.tencent.com>
2021-10-15 18:54:37 +0800

    delete

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php $this->need('header.php'); ?>
-<link rel="stylesheet" href="<?php $this->options->themeUrl('css/404.css'); ?>">
-
-<div class="site-name404">
-    <img class="img404" src="<?php $this->options->themeUrl('img/404.png'); ?>">
-</div>
-
-<?php $this->need('footer.php'); ?>
