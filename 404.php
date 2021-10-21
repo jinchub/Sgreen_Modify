77a7af45019a77705a166713fc21034b77057194
jinc <41d_1054542382_jinc@git.cloud.tencent.com>
2021-10-21 15:44:02 +0800

    update

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="<?php $this->options->themeUrl('css/404.css'); ?>">
 <content class="main" style="width:100%;height: 100px;position: absolute;padding: 70px 0px 50px 0px;">
     <div class="site-name404">
-        <p style="font-size: 22px;font-family: 华文彩云;color: #009fd9;filter: drop-shadow(0px 0px 2px #009fd9);">请求内容未找到<br>或许已经被删除</p>
+        <p style="font-size: 22px;font-family: 华文彩云;color: #009fd9;filter: drop-shadow(0px 0px 2px #009fd9);">请求内容未找到<br>不存在或已删除</p>
     </div>
 </content>
 
